eaedb57e1ae3da5f7e8c6d7d99f9ff5f619046a7
Alec HARMON MACBOOK <alecblumenfeld@gmail.com>
2014-11-14 14:27:06 -0500

    random

--- Sudoku.java
+++ Sudoku.java
@@ -14,6 +14,9 @@ class Point{
      public String toString() {
         return "X: " + x + "  Y:" + y + "  posssible:"+ posssible;
     }
+    public List<Integer> getPoint(){
+        return this.posssible;     
+    } 
     
 }
 class Sudoku
@@ -119,69 +122,68 @@ class Sudoku
                   }
               }
           }
-          System.out.println(zerolist);
 
            
           
-        // int ORGINALGRID[][] =Grid;
-        // int CURRENTGRID[][] =Grid;
-        // int NEWGRID[][] =Grid;
-        // double threshold = 1.0;
-        // double coolingrate = 0.003;
-        // /// randomize 
+        int ORGINALGRID[][] =Grid;
+        int CURRENTGRID[][] =Grid;
+        int NEWGRID[][] =Grid;
+        double threshold = 1.0;
+        double coolingrate = 0.003;
+        /// randomize 
         
-        // while(flag==true){
-        //     int count = 0;
-        //     flag =false;
-        //             for (int x =0 ; x < zerolist.size() ;x++ ) {
-        //                 int randpoint =  (int)(Math.random()*zerolist.size());
-        //                 NEWGRID = CURRENTGRID;
-        //                 CURRENTGRID[zerolist.get(randpoint).x][zerolist.get(randpoint).y] = (int)(Math.random()*8 )+1;
-        //                 // for(int k = 1; k<N+1;k++){
-        //                 //     int lastScore = this.evaluate();
-        //                 //     int temp = Grid[zerolist.get(randpoint).x][zerolist.get(randpoint).y];
-        //                 //     Grid[zerolist.get(x)][zerolist.get(x+1)] =k;
-        //                 //     int nextscore = this.evaluate();
-        //                 //     if (lastScore >nextscore) {
-        //                 //           Grid[zerolist.get(x)][zerolist.get(x+1)]=temp ;
-        //                 //      } 
-        //                 //      else{
-        //                 //         flag =true;
-        //                 //      }
+        while(flag==true){
+            int count = 0;
+            flag =false;
+                    for (int x =0 ; x < zerolist.size() ;x++ ) {
+                        int randpoint =  (int)(Math.random()*zerolist.size());
+                        NEWGRID = CURRENTGRID;
+                        CURRENTGRID[zerolist.get(randpoint).x][zerolist.get(randpoint).y] = (int)(Math.random()*8 )+1;
+                        // for(int k = 1; k<N+1;k++){
+                        //     int lastScore = this.evaluate();
+                        //     int temp = Grid[zerolist.get(randpoint).x][zerolist.get(randpoint).y];
+                        //     Grid[zerolist.get(x)][zerolist.get(x+1)] =k;
+                        //     int nextscore = this.evaluate();
+                        //     if (lastScore >nextscore) {
+                        //           Grid[zerolist.get(x)][zerolist.get(x+1)]=temp ;
+                        //      } 
+                        //      else{
+                        //         flag =true;
+                        //      }
 
-        //                 // }
+                        // }
+                                    point tempPoint = zerolist[x];
+                                    for(int k = 1; k<tempPoint.posssible.size();k++){
+                                    int lastScore = this.evaluate();
+                                    int temp = CURRENTGRID[zerolist.get(randpoint).x][zerolist.get(randpoint).y];
+                                    CURRENTGRID[zerolist.get(randpoint).x][zerolist.get(randpoint).y] =k;
+                                    int nextscore = this.evaluate();
+                                    System.out.println(lastScore + ">" + nextscore + "      K: "+k);
+                                    if (lastScore <nextscore) {
+                                          CURRENTGRID[zerolist.get(randpoint).x][zerolist.get(randpoint).y]=temp ;
+                                          count++;
+                                          threshold = threshold - (1*coolingrate);
+                                          System.out.println(threshold);
+                                          if (threshold >0.1) {
+                                              CURRENTGRID = NEWGRID;
+                                          }
+                                          else {
+                                              flag =true;
+                                          }
 
-        //                             for(int k = 1; k<10;k++){
-        //                             int lastScore = this.evaluate();
-        //                             int temp = CURRENTGRID[zerolist.get(randpoint).x][zerolist.get(randpoint).y];
-        //                             CURRENTGRID[zerolist.get(randpoint).x][zerolist.get(randpoint).y] =k;
-        //                             int nextscore = this.evaluate();
-        //                             System.out.println(lastScore + ">" + nextscore + "      K: "+k);
-        //                             if (lastScore <nextscore) {
-        //                                   CURRENTGRID[zerolist.get(randpoint).x][zerolist.get(randpoint).y]=temp ;
-        //                                   count++;
-        //                                   threshold = threshold - (1*coolingrate);
-        //                                   System.out.println(threshold);
-        //                                   if (threshold >0.1) {
-        //                                       CURRENTGRID = NEWGRID;
-        //                                   }
-        //                                   else {
-        //                                       flag =true;
-        //                                   }
-
-        //                                    if (count >150) {
-        //                                     CURRENTGRID = ORGINALGRID;
-        //                                     count = 0;
-        //                                   }
-        //                                   break;
-        //                              } 
-        //                         }
+                                           if (count >150) {
+                                            CURRENTGRID = ORGINALGRID;
+                                            count = 0;
+                                          }
+                                          break;
+                                     } 
+                                }
                             
                         
                         
-        //         }
-        //  }
-        //  Grid = CURRENTGRID;
+                }
+         }
+         Grid = CURRENTGRID;
     }
 
 
